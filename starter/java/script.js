2dff4071c610da7968dce999c5dfab825296d26f
amaal abdulle <amaalabdelmuid@gmail.com>
2023-12-12 17:33:09 +0000

    working planner with color and localstorage

--- starter/java/script.js
+++ starter/java/script.js
@@ -3,50 +3,59 @@ dayjs.extend(window.dayjs_plugin_advancedFormat)
 var currentDay = dayjs();
 $('#currentDay').text(currentDay.format('dddd MMMM  Do'));
 
+var currentHourBlock; 
 // past, present & future
 var currentTime = dayjs();
 console.log('Current Time:', currentTime.format('HH:mm:ss'));
 
 function hourTrackerColor() {
     var currentHour = dayjs().hour();
-    console.log(currentHour); // getting the current hour
+    console.log('Current Hour: ' + currentHour); // getting the current hour
 
     // looping over each timeblock
-    $('.time-blocl').each(function() {
-        var currentHourBlock = parseInt($(this).attr("id").split("-")[1]); // dont get this much
+    $('.time-block').each(function() {
+        currentHourBlock = parseInt($(this).attr('id').split('-')[1]);
+        console.log('current hour block: ' + currentHourBlock);
 
         if (currentHourBlock < currentHour) {
-            $(this).addClass('past');
+            $(this).addClass('past')
         } else if (currentHourBlock === currentHour) {
             $(this).removeClass('past');
             $(this).addClass('present');
         } else {
+            $(this).removeClass("past");
             $(this).removeClass('present');
             $(this).addClass('future');
         }
-    })
+    });
 }
-
 hourTrackerColor ();
 
 // allowing the user to enter an event when they click a timeblock
-
 // saving to local storage
 
 const saveBtn = $('.saveBtn');
 
 
 saveBtn.on('click', function() {
-    event.preventDefault();
+    // event.preventDefault();
     console.log('i am clicked');
-    const text = $('textarea').val;
-    const item = 'hour: ' + hourCurrent; // will not work untill this is defined
+    
+    const text = $(this).siblings('.background').val();
+    var item = $(this).parent().attr('id');
     localStorage.setItem(item, text);
 
+    console.log($(this).siblings('.background').val());
+    console.log($(this).parent().attr('id'));
+    
     alert ('saved to local storage');
 });
 
 
+
+for (var i=9; i < 17; i++) {
+    $('#hour-'+i +' .background').val(localStorage.getItem('hour-' +i));
+}
 
 
 // const localStorageData = [{
